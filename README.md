ac213a341816ae915c06be10e0c7956ff19e61c1
Howard Yeend <howard.yeend@gmail.com>
2020-03-09 16:21:11 +0000

    Now adds project name and demo link to README.md, and can now be run from outside tidycrap directory. Updated tidycrap's readme too

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
-# tidycrap
-Takes a freshly made create-react-app project, strips out some stuff, and adds in github-pages support
+# Tidycrap
 
-i.e. tidies CRA, and adds Pages. Hence tidyCRAp.
+A node-cli script which takes a freshly made create-react-app project, strips out some stuff, and adds in github-pages support, along with the abilty to deploy to github pages by simply running `yarn deploy`.
 
-To see if this is for you, I suggest you run it on a freshly create-react-app'd directory and then run git diff and yarn start to check it's all working.
+I found that I was always doing the same 'tidying' after running CRA, so this is my way of automating that work.
 
-# What it does
+It is extremely not recommended to run it on non-empty projects, although you'll be able to revert the damage.
+
+Maybe a future version will try to detect if it's running in a repo with real work in it.
+
+To see if this is for you, I suggest you run it on a freshly create-react-app'd directory and then run git diff and yarn start.
+
+## What it does
 
 * Adds the gh-pages package
 * Modifies package.json with gh-pages homepage and deploy scripts
+* Modifies README.md with the project name and a link to the homepage
 * Deletes CRA logo files
 * Deletes manifest.json and references to it in index.html
 * Deletes serviceWorker.js and references to it in index.js
@@ -16,14 +22,20 @@ To see if this is for you, I suggest you run it on a freshly create-react-app'd 
 * Replaces App functional component with a PureComponent based one
 * Creates a commit with the message "tidycrap"
 
-# Setup
+## Setup
 
 ```
 chmod +x tidycrap.js
 ```
 
-# Usage
+## Usage
 
 ```
-$ ./tidycrap.js -h
+Usage: ./tidycrap.js [options]
+
+Options:
+  -u, --username <name>  Github username
+  -p, --path <path>      Path to the repo relative to tidycrap e.g. -p ../awesome-project
+  -n, --name <name>      Name of this repo (default: derived from path)
+  -h, --help             output usage information
 ```
--- tidycrap.js
+++ tidycrap.js
@@ -4,7 +4,7 @@
 const program = require('commander');
 program.storeOptionsAsProperties(false);
 program.requiredOption('-u, --username <name>', 'Github username');
-program.requiredOption('-p, --path <path>', 'Path to the repo');
+program.requiredOption('-p, --path <path>', `Path to the repo relative to ${__dirname} e.g. -p ../awesome-project`);
 program.option('-n, --name <name>', 'Name of this repo (default: derived from path)');
 program.parse(process.argv);
 const options = program.opts();
@@ -21,19 +21,18 @@ exec('yarn add gh-pages', {
   cwd: options.path
 }, (err, stdout, stderr) => {
   if (err) {
-    // node couldn't execute the command
-    console.error(stderr);
+    console.log(err, stdout, stderr);
     return;
   }
-
   console.log('OK');
 
+  const homepage = `https://${options.username}.github.io/${name}`;
 
   console.log('Modifying package.json');
   // Modify package.json with gtihub pages deets.
   const packageJsonLocation = `${options.path}/package.json`;
   const packageJson = require(packageJsonLocation);
-  packageJson.homepage = `https://${options.username}.github.io/${name}`;
+  packageJson.homepage = homepage;
   packageJson.scripts = packageJson.scripts || {};
   packageJson.scripts.predeploy = 'yarn run build';
   packageJson.scripts.deploy = 'gh-pages -b master -d build';
@@ -54,9 +53,21 @@ exec('yarn add gh-pages', {
     fs.unlink(`${options.path}/src/serviceWorker.js`, (err) => { err && console.error(err) });
     fs.writeFile(`${options.path}/src/App.css`, "", (err) => { err && console.error(err) });
     fs.writeFile(`${options.path}/src/index.css`, "", (err) => { err && console.error(err) });
-    fs.createReadStream('./templates/index.html').pipe(fs.createWriteStream(`${options.path}/public/index.html`));
-    fs.createReadStream('./templates/App.js').pipe(fs.createWriteStream(`${options.path}/src/App.js`));
-    fs.createReadStream('./templates/index.js').pipe(fs.createWriteStream(`${options.path}/src/index.js`));
+    fs.createReadStream(`${__dirname}/templates/index.html`).pipe(fs.createWriteStream(`${options.path}/public/index.html`));
+    fs.createReadStream(`${__dirname}/templates/App.js`).pipe(fs.createWriteStream(`${options.path}/src/App.js`));
+    fs.createReadStream(`${__dirname}/templates/index.js`).pipe(fs.createWriteStream(`${options.path}/src/index.js`));
+    console.log("OK");
+
+    console.log("Updating README.md with project title and demo link");
+    const data = fs.readFileSync(`${options.path}/README.md`);
+    const fd = fs.openSync(`${options.path}/README.md`, 'w+');
+    const capitalisedName = name.toUpperCase().substring(0, 1) + name.substring(1).toLowerCase();
+    const insertIntoReadme = `# ${capitalisedName}\n\nCheck out the [project demo](${homepage}).\n\n## Create React App stuff\n\n`;
+    fs.writeSync(fd, insertIntoReadme, 0, insertIntoReadme.length, 0)
+    fs.writeSync(fd, data, 0, data.length, insertIntoReadme.length)
+    fs.close(fd, (err) => {
+      if (err) console.log(err);
+    });
     console.log("OK");
 
     console.log('Committing');
@@ -71,4 +82,5 @@ exec('yarn add gh-pages', {
       console.log('OK');
     });
   });
+
 });
